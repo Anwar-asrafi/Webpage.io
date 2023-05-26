9d5d004f8e7d1d9f3acc4a751b2dd01d9be9dd14
Anwar Ashrafi <illegal786.myth@gmail.com>
2023-05-26 22:28:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+
+var password = "NOKIA";
+var response;
+var entryCount = 0;
+var entryLimit = Infinity;
+var error = false;
+
+while(response != password && !error){
+     if(entryCount < entryLimit){
+          response = window.prompt("Enter Password");
+          entryCount++;
+     } else {
+          error = true;
+     }
+}
+
+if(error){
+     alert("incorrect,Try agian");
+} else {
+     alert("Correct Answer✔️");
+}
